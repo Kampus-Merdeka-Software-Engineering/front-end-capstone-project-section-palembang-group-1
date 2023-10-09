f76c667dc964c61d1a2f9c833ac3057d22d611ca
Shabila Ocktavia <129844708+bils05@users.noreply.github.com>
2023-10-09 17:53:01 +0700

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -38,7 +38,7 @@ closeProfile.onclick = () => {
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  const cartContainer = document.querySelector("cart");
+  const cartContainer = document.querySelector(".cart");
   const addToCartButton = document.getElementById("buyButton");
 
   // Fungsi untuk menambahkan produk ke keranjang
